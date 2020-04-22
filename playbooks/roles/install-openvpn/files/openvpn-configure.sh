9264b9e19ba7b79931b252b2097e856071df3b6a
Andrej Babic <hello@bluecap.se>
2020-04-22 20:10:25 +0200

    Bugfix: Modified VPN configuration

---
+++ playbooks/roles/install-openvpn/files/openvpn-configure.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/expect -f
+
+set timeout -1
+
+spawn bash /tmp/openvpn-install.sh
+
+expect "IPv4 address"
+send -- "\r"
+
+expect "Protocol"
+send -- "\r"
+
+expect "Port"
+send -- "\r"
+
+expect "DNS"
+send -- "\r"
+
+expect "Client name"
+send -- "\r"
+
+expect "Press any key to continue..."
+send -- "\r"
+
+expect eof
